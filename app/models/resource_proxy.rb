7a27f5b50abb198a7cbbdde208ec25a81fb23f91
Jason Stirnaman <stirnamanj@gmail.com>
2013-01-30 17:09:56 -0600

    Fixed search method, essentially an alias to EzProxy links and Mechanize links_with. Accepts string or regexp and converts a string to a simple regexp.

--- app/models/resource_proxy.rb
+++ app/models/resource_proxy.rb
@@ -1,15 +1,18 @@
 class ResourceProxy
 
   include Ezproxy #api/ezproxy.rb
-
+  attr_accessor :client
+  
   def initialize
     @client = Ezproxy::Client.new
   end
   
   def search(query)
-  #Returns search results for query, e.g. search("pubmed")
+  # Accept string or regexp, but if string convert to regexp.
+  query = query.class == Regexp ? query : %r/#{query}/
+  #Returns results matching query, e.g. search("pubmed")
     #Array of Mechanize::Page::Links objects
-    @q = @client.search({:name => %r/.*#{query}.*/})
+    @q = client.links({:text => query})
   end
  
 end
